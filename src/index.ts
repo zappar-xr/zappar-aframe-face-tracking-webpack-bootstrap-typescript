24abd5f8d32f067697bff383d5c754c73c083e09
Zappar <hello@zappar.com>
2022-03-15 18:25:05 +0000

    Minimal webpack project for Zappar AFrame face-tracking

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import * as AFRAME from "aframe";
+import * as ZapparAFrame from "@zappar/zappar-aframe";
+
+console.log("Using AFRAME", AFRAME.version);
+console.log("Using ZapparAFrame", ZapparAFrame);
+
+window.addEventListener("load", setup);
+
+function setup() {
+
+}
